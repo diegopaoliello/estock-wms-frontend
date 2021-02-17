38cc8692fab7bb79e74a916e687ba3553a3da7ae
Diego <diegopaoliello@gmail.com>
2021-02-17 18:03:58 -0300

    Alteração da porta do servidor

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || "8081";
+const PORT = process.env.PORT || "8080";
 
 app.use(express.static(__dirname + "/dist/estock-wms-app"));
 

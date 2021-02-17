9026e139ace6ad1cfe738ca64ccf38622ad8f4bd
Diego <diegopaoliello@gmail.com>
2021-02-17 17:54:46 -0300

    Implementação do servidor Node da aplicação front end

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || "8081";
+
+app.use(express.static(__dirname + "/dist/estock-wms-app"));
+
+app.get("/*", (req, res) => {
+  res.sendFile(__dirname + "/dist/estock-wms-app/index.html");
+});
+
+app.listen(PORT, () => {
+  console.log("Servidor iniciado na porta: " || PORT);
+});

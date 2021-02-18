b296bf8e4389d867f997e0cc12bdc862265a52b1
Diego <diegopaoliello@gmail.com>
2021-02-18 14:23:51 -0300

    Alteração server_url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURLBase: 'http://api-estock.herokuapp.com',
+  apiURLBase: 'https://api-estock.herokuapp.com',
   clientId: 'my-angular-app',
   clientSecret: '@321',
   obterTokenUrl: '/api/oauth/token',

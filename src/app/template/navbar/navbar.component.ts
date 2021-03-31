8abeba6b4b2a790f047503c6052c22e3f65774da
Diego <diegopaoliello@gmail.com>
2021-03-30 21:25:11 -0300

    Alteração no layout para o modelo sbAdmin2

--- src/app/template/navbar/navbar.component.ts
+++ src/app/template/navbar/navbar.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from 'src/app/auth.service';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-  constructor() {}
+  usuarioLogado: string;
 
-  ngOnInit(): void {}
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.usuarioLogado = this.authService.getUsuarioAutenticado();
+  }
 }
